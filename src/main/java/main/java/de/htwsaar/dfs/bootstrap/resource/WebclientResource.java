32f82bebc478a30abe102eea79fba57e6483abb6
Julian Einspenner <julianeinspenner@yahoo.de>
2018-08-11 02:18:58 +0200

    Webclient funktioniert nun auch, wenn der Server als jar exportiert wurde

--- src/main/java/main/java/de/htwsaar/dfs/bootstrap/resource/WebclientResource.java
+++ src/main/java/main/java/de/htwsaar/dfs/bootstrap/resource/WebclientResource.java
@@ -1,51 +1,116 @@
 package main.java.de.htwsaar.dfs.bootstrap.resource;
 
-import java.awt.Image;
-import java.io.File;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+import javax.imageio.ImageIO;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * The webclient resource class of Projekt-DFS backend.
+ * It makes possible to load the login-screen via browser, does not matter
+ * which device you are using
+ * @author Julian Einspenner
+ *
+ */
 @Path("/")
 public class WebclientResource {
 
+	
+	/**
+	 * Loads the login-screen via browser
+	 * @return the login-screen of the backend
+	 */
 	@GET
 	@Path("webclient")
 	@Produces(MediaType.TEXT_HTML)
-	public File showWebclient() {
-		return new File("./webclient/index.html");
+	public String showWebclient() {
+		return readFile("index.html");
 	}
 	
-	
+	/**
+	 * Loads graphics for the webclient to have a GUI while browsing  
+	 * @param name is the name of a graphic file
+	 * @return the graphic
+	 */
 	@GET
 	@Path("webclient/graphics/{name}")
 	@Produces("image/png")
-	public File getGraphic(@PathParam("name") String name) {
-		return new File("./webclient/graphics/" + name);
+	public BufferedImage getGraphic(@PathParam("name") String name) {
+		return readImage(name);
 	}
 	
-	
+	/**
+	 * Loads the css file to make the webclient more beauty
+	 * @return the css-file as a string
+	 */
 	@GET
 	@Path("webclient/files/style.css")
 	@Produces("text/css")
-	public File getStyle() {
-		System.out.println("testS");
-		return new File("./webclient/style.css");
+	public String getStyle() {
+		return readFile("style.css");
 	}
 	
-	
+	/**
+	 * Loads the javascript-file which includes the logic of the webclient
+	 * @return the javascript-file
+	 */
 	@GET
 	@Path("webclient/files/skripte.js")
 	@Produces("text/javascript")
-	public File getScripts() {
-		return new File("./webclient/skripte.js");
+	public String getScripts() {
+		return readFile("skripte.js");
 	}
 	
 	
 	
+	/**
+	 * Reads a file and gives it back to the client as a string.
+	 * Necessary for having an exported jar-file of the server.
+	 * @param name is the name of the file which is going to be read
+	 * @return the file content as a string
+	 */
+	private static String readFile(String name) {
+		BufferedReader br = new BufferedReader(
+							new InputStreamReader(
+							WebclientResource.class.getResourceAsStream("/"+name)));
+		
+		String line;
+		String fileAsString = "";
+        try {
+			while((line=br.readLine())!= null){
+			    fileAsString += line + "\n"; 
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
+        System.out.println(fileAsString);
+        return fileAsString;
+	}
 	
-	
+	/**
+	 * Reads an image and gives it back to the client.
+	 * Necessary for having an exported jar-file of the server.
+	 * @param name is the name of the image which is going to be read
+	 * @return the image
+	 */
+	private static BufferedImage readImage(String name) {
+		BufferedImage img = null;
+		
+		try {
+			img = ImageIO.read(WebclientResource.class.getResource("/graphics/" + name));
+		} catch (IOException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		
+        return img;
+	}
 }
