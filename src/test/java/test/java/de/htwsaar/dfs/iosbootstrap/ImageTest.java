5c0d613f65f5feb429f83be31e9193482f904743
Thomas Spanier <thomas.spanier24@googlemail.com>
2018-09-03 12:51:40 +0200

    Updated JUnit tests

--- src/test/java/test/java/de/htwsaar/dfs/iosbootstrap/ImageTest.java
+++ src/test/java/test/java/de/htwsaar/dfs/iosbootstrap/ImageTest.java
@@ -1,16 +1,13 @@
 package test.java.de.htwsaar.dfs.iosbootstrap;
 
 import static org.junit.Assert.*;
-
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
-
 import javax.imageio.ImageIO;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,6 +15,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import main.java.de.htwsaar.dfs.model.*;
+import main.java.de.htwsaar.dfs.utils.StaticFunctions;
 
 public class ImageTest {
 
@@ -41,7 +39,7 @@ public class ImageTest {
 		Date date = new Date();
 		LinkedList<String> tagList = new LinkedList<String>();
 		
-		img = ImageIO.read(new File("twins.jpg"));
+		img = ImageIO.read(new File("Classdiagram.jpg"));
 		photographer = "Thomas";
 		tagList.add("babys");
 		bt.createImage(img, Bootstrap.getUser("imageTestUser1").getName(), "twins.jpg", photographer, date, tagList);
@@ -57,7 +55,7 @@ public class ImageTest {
 		
 		
 		
-		img = ImageIO.read(new File("coins.jpg"));
+		img = ImageIO.read(new File("Classdiagram.jpg"));
 		photographer = "amazon";
 		tagList.removeIf(s -> true);
 		tagList.add("Kaufbelege");
@@ -77,43 +75,34 @@ public class ImageTest {
 	}
 
 	
-	@Test
-	public void testGetImageContainers() {
-		try {
-			ArrayList<ImageContainer> ics = bt.getAllImageContainers("imageTestUser2");
-			assertEquals(1, ics.size());
-			
-			ics = bt.getAllImageContainers("imageTestUser1");
-			assertEquals(2, ics.size());
-			
-			System.out.println(ics.get(1).getTags());
-		} catch (ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	@Test
-	public void testDeleteImage() {
-		bt.deleteImage("imageTestUser1", "twins.jpg");
-		ArrayList<ImageContainer> ics;
-		try {
-			ics = bt.getAllImageContainers("imageTestUser1");
-			
-			assertEquals(1, ics.size());
-		} catch (ClassNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
 	
 	
 	@Test
-	public void testGetThumbnail() {
+	public void testGetImage() {
 		try {
 			ImageContainer ic = bt.loadImageContainer("imageTestUser1", "coins.jpg");
+			Point p = StaticFunctions.hashToPoint("imageTestUser1", "coins.jpg");
+			assertEquals(p.getX(), ic.getCoordinate().getX(), 0.001);
+			assertEquals(p.getY(), ic.getCoordinate().getY(), 0.001);
+			
+			assertEquals(2, Bootstrap.getUser("imageTestUser1").getImageList().parallelStream().count());
+			
+			//All files created?
+			File userFolder = new File("images/imageTestUser1");
+			assertEquals(6, userFolder.listFiles().length);
+			File f = new File("images/imageTestUser1/coins.data");
+			assertEquals(true,f.exists());
+			f = new File("images/imageTestUser1/twins.data");
+			assertEquals(true,f.exists());
+			
+			//Are all tags correct
+			LinkedList<String> tagList = new LinkedList<String>();
+			tagList.add("Kaufbelege");
+			tagList.add("money");
+			assertEquals(true, ic.getTagList().containsAll(tagList));
+			
+			//Are the paths correct
+			assertEquals("images/imageTestUser1/coins", ic.getPath());
 			assertEquals("images/imageTestUser1/coins_thumbnail.jpg", ic.getThumbnailPath());
 			
 			
@@ -121,21 +110,87 @@ public class ImageTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 	
 	
 	@Test
-	public void testGetPath() {
+	public void testDoubleName() {
+		BufferedImage img;
+		String photographer;
+		Date date = new Date();
+		LinkedList<String> tagList = new LinkedList<String>();
+		
 		try {
-			ImageContainer ic = bt.loadImageContainer("imageTestUser1", "coins.jpg");
-			assertEquals("images/imageTestUser1/coins", ic.getPath());
-			
-			
+			img = ImageIO.read(new File("Classdiagram.jpg"));
+			photographer = "Thomas Spanier";
+			tagList.add("rich");
+			tagList.add("money");
+			bt.createImage(img, "imageTestUser1", "coins.jpg", photographer, date, tagList);
+			Point p = StaticFunctions.hashToPoint("imageTestUser1", "coins0.jpg");
+			assertEquals("coins0.jpg", bt.loadImageContainer("imageTestUser1", "coins0.jpg").getImageName());
+			assertEquals(true, bt.loadImageContainer("imageTestUser1", "coins0.jpg").getTagList().contains("rich"));
+			assertEquals(false, bt.loadImageContainer("imageTestUser1", "coins0.jpg").getTagList().contains("Kaufbelege"));
+			//Coordinate
+			assertEquals(p.getX(), bt.loadImageContainer("imageTestUser1", "coins0.jpg").getCoordinate().getX(), 0.001);
+			assertEquals(p.getY(), bt.loadImageContainer("imageTestUser1", "coins0.jpg").getCoordinate().getY(), 0.001);
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
+	@Test
+	public void testGetAllImageContainers() {
+		try {
+			ArrayList<ImageContainer> ics = bt.getAllImageContainers("imageTestUser2");
+			assertEquals(1, ics.size());
+			LinkedList<String> tagList = new LinkedList<String>();
+			tagList.add("UML");
+			tagList.add("software");
+			tagList.add("diagram");
+			assertEquals(true, ics.get(0).getTagList().containsAll(tagList));
+			ics = bt.getAllImageContainers("imageTestUser1");
+			assertEquals(2, ics.size());
+			//Are all tags correct
+			tagList = new LinkedList<String>();
+			tagList.add("Kaufbelege");
+			tagList.add("money");
+			assertEquals(true, ics.get(1).getTagList().containsAll(tagList));
 		} catch (ClassNotFoundException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 	
+		
+	@Test
+	public void testDeleteImage() {
+		bt.deleteImage("imageTestUser1", "twins.jpg");
+		ArrayList<ImageContainer> ics;
+		try {
+			
+			//All files deleted?
+			File userFolder = new File("images/imageTestUser1");
+			assertEquals(3, userFolder.listFiles().length);
+			File f = new File("images/imageTestUser1/twins.data");
+			assertEquals(false,f.exists());
+			f = new File("images/imageTestUser1/twins.jpg");
+			assertEquals(false,f.exists());
+			f = new File("images/imageTestUser1/twins_thumbnail.jpg");
+			assertEquals(false,f.exists());
+			
+			ics = bt.getAllImageContainers("imageTestUser1");
+			assertEquals(1, ics.size());
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
